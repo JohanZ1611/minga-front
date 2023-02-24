605842d01bd72bf495d0a7b229df9c2cfc5247ba
JohanZ1611 <121687526+JohanZ1611@users.noreply.github.com>
2023-02-24 15:34:27 -0500

    M 01 (#2)
    * normalizacion terminada
    
    * diseño hero terminado

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './navBar.css'
+export default function NavBar() {
+  return (
+    <div className='cont-nav'>
+        <img src="/images/Menu.svg" alt="menu" />
+        <img src="/images/Logo.svg" alt="logo" />
+    </div>
+    
+  )
+}
