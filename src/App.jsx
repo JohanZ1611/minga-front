34d77fdf1585916a1eb962500c671ba018710bbd
Johan Zuluaga <johan16zulu@gmail.com>
2023-02-25 15:48:08 -0500

    mejoras en el responsive del register

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Register from './components/Register/Register';
 function App() {
   return (
     <div className="App">
-      <Hero/>
-      {/* <Register/> */}
+      {/* <Hero/> */}
+      <Register/>
     </div>
   );
 }
