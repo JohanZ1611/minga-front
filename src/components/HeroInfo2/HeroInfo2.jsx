15afd9cbbf74858252e14c5112f05169c231c6f3
Johan Zuluaga <johan16zulu@gmail.com>
2023-02-26 11:19:24 -0500

    fix2

--- src/components/HeroInfo2/HeroInfo2.jsx
+++ src/components/HeroInfo2/HeroInfo2.jsx
@@ -7,15 +7,9 @@ export default function HeroInfo() {
         <img src="/images/img1-info-hero.png" alt="she" className='img1-info'/>
         <img src="/images/img2-info-hero.png" alt="minga" className='img2-info'/>
         <div className='cont-info2-hero'>
-<<<<<<< HEAD
             <h2 className='h2-info2-hero'>One Piece</h2>
             <p className='p1-info2-hero'>Manga</p>
             <p className='p2-info2-hero'>Monkey D. Luffy refuses to let anyone stand in his way to become King of the Pirates. He launches into the seas and will become a captain willing to never give up until he gets hold of the greatest treasure in history: the legendary One Piece.</p>
-=======
-            <h2>One Piece</h2>
-            <p>Manga</p>
-            <p>Monkey D. Luffy refuses to let anyone stand in his way to become King of the Pirates. He launches into the seas and will become a captain willing to never give up until he gets hold of the greatest treasure in history: the legendary One Piece.</p>
->>>>>>> 605842d01bd72bf495d0a7b229df9c2cfc5247ba
         </div>
     </div>
   )
