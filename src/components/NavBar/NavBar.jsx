d8da12b5ae3173befa83058eba9c99cd67ae0f6a
Johan Zuluaga <johan16zulu@gmail.com>
2023-02-26 10:19:02 -0500

    funcionalidad navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './navBar.css'
+import {useState} from 'react'
+
+
 export default function NavBar() {
+  const [isOpen,setIsOpen] = useState(false)
   return (
     <div className='cont-nav'>
-        <img src="/images/Menu.svg" alt="menu" className='img-nav'/>
+        <div className={`nav_items ${isOpen && "open"}`}>
+          <a href="#">Home</a>
+          <a href="#">Upcoming Event</a>
+          <a href="#">Past Event</a>
+          <a href="#">Contact</a>
+          <a href="#">Stats</a>
+        </div>
+        <div className={`nav_toggle ${isOpen && "open"}`} onClick={()=> setIsOpen(!isOpen)} >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <img src="/images/Logo.svg" alt="logo" className='img-nav-web' />
         <img src="/images/logo-mobile.svg" alt="logo" className='img-nav-mobile' />
     </div>
